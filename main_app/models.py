87ffe98a08485d645540a1404f9f4ff75ab427c7
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-04-26 15:27:22 -0700

    extended my user to have a one to one relationship with a current balance model, also added this model to the admin file for admin access

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Current_Balance(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    amount = models.FloatField()
