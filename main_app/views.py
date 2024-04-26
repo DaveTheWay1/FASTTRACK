5e91bd5a51f8c4991ed902ab9b7014202d37f191
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-04-26 14:41:17 -0700

    created path handlers to handle a home render, user logout, user sign up

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return render(request, 'home.html')
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+        # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in via code
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid sign up - try again'
+            # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
