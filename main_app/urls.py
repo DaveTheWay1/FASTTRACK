c4d4e77318dcfabb6352721cc803384cf22b2bde
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-04-28 17:51:51 -0700

    create a route for adding a balance and a route handler for that route

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/logout/', views.user_logout, name='user_logout'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('user/<int:user_id>/add_current_balance/', views.add_current_balance, name='add_current_balance'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,31 +3,40 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from .forms import CurrentBalanceForm
+from .models import Current_Balance
 
 @login_required
 def home(request):
-    return render(request, 'home.html')
+    cur_balance = Current_Balance.objects.filter(user=request.user).first()
+    add_balance_form = CurrentBalanceForm
+    return render(request, 'home.html', {'cur_balance':cur_balance, 'add_balance_form':add_balance_form})
 
 @login_required
 def user_logout(request):
     logout(request)
-    return render(request, 'home.html')
+    return redirect('/accounts/login/')
 
 def signup(request):
     error_message = ''
     if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
         form = UserCreationForm(request.POST)
         if form.is_valid():
-        # This will add the user to the database
             user = form.save()
-            # This is how we log a user in via code
             login(request, user)
             return redirect('home')
         else:
             error_message = 'Invalid sign up - try again'
-            # A bad POST or a GET request, so render signup.html with an empty form
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
+
+
+@login_required
+def add_current_balance(request, user_id):
+    form = CurrentBalanceForm(request.POST)
+    if form.is_valid():
+        new_current_balance = form.save(commit=False)
+        new_current_balance.user_id = user_id
+        new_current_balance.save()
+    return redirect('home')
